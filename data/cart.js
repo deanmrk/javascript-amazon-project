4f284f3350f048154eca80225ad2620ee4ad385e
Deanmark <deansy0718@gmail.com>
2026-01-29 14:34:32 +0800

    started the checkout page

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,17 @@
-export const cart = []; //EXPORT THE CART VARIABLE TO ACCESS IN THE amazon.js
+export const cart = [  //EXPORT THE CART VARIABLE TO ACCESS IN THE amazon.js
+    {
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', //default value
+        quantity: 2
+    },
+    {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d', //default value
+        quantity: 1
+    },
+    {
+        productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e', //default value
+        quantity: 3
+    },
+];
 
 export function addtoCart(productId, quantity) {
   let matchingItem;
---
+++ scripts/checkout.js
@@ -0,0 +1,96 @@
+import { cart } from "../data/cart.js";
+import { products } from "../data/products.js"; //need to import this to find the data of that products using product id
+
+const container = document.querySelector('.js-order-summary')
+
+let itemHTML = '';
+cart.forEach( (cartItem) => {
+
+    const productId = cartItem.productId;
+
+    let matchingProduct; //create an empty variable to store the result
+    products.forEach( (item) => { //check the product data using id
+        if (item.id === productId) {
+            matchingProduct = item; //save the item in the empty variable u created earlier
+        }
+    });
+
+    
+    itemHTML += `
+    <div class="cart-item-container">
+        <div class="delivery-date">
+            Delivery date: Wednesday, June 15
+        </div>
+
+        <div class="cart-item-details-grid">
+            <img class="product-image"
+            src="${matchingProduct.image}">
+
+            <div class="cart-item-details">
+            <div class="product-name">
+                ${matchingProduct.name}
+            </div>
+            <div class="product-price">
+                $${(matchingProduct.priceCents / 100)}
+            </div>
+            <div class="product-quantity">
+                <span>
+                Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+                </span>
+                <span class="update-quantity-link link-primary">
+                Update
+                </span>
+                <span class="delete-quantity-link link-primary">
+                Delete
+                </span>
+            </div>
+            </div>
+
+            <div class="delivery-options">
+            <div class="delivery-options-title">
+                Choose a delivery option:
+            </div>
+
+            <div class="delivery-option">
+                <input type="radio" class="delivery-option-input"
+                name="delivery-option-2">
+                <div>
+                <div class="delivery-option-date">
+                    Tuesday, June 21
+                </div>
+                <div class="delivery-option-price">
+                    FREE Shipping
+                </div>
+                </div>
+            </div>
+            <div class="delivery-option">
+                <input type="radio" checked class="delivery-option-input"
+                name="delivery-option-2">
+                <div>
+                <div class="delivery-option-date">
+                    Wednesday, June 15
+                </div>
+                <div class="delivery-option-price">
+                    $4.99 - Shipping
+                </div>
+                </div>
+            </div>
+            <div class="delivery-option">
+                <input type="radio" class="delivery-option-input"
+                name="delivery-option-2">
+                <div>
+                <div class="delivery-option-date">
+                    Monday, June 13
+                </div>
+                <div class="delivery-option-price">
+                    $9.99 - Shipping
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+    `;
+});
+
+container.innerHTML = itemHTML;
